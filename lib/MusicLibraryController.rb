2ab2717a959c1b7a093557874294b535bb233802
Milan Parmar <milan_parmar@hotmail.co.uk>
2020-02-03 16:38:31 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -83,7 +83,7 @@ class MusicLibraryController
   
   def play_song 
     puts "Which song number would you like to play?"
-    input = gets.chomp
+    input = gets.to_i
     if input == list_songs 
       puts "Playing #{song.name} by #{artist.name}"
     end 
